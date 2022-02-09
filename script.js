d3ebb86dd9682ab9be6a16c1191bfdc8885cfaad
Kjasuquo <okoasuquo@yahoo.com>
2022-02-09 01:12:34 +0100

    js added

---
+++ script.js
@@ -0,0 +1,6 @@
+function sendMail(){
+    const subject = document.getElementById('subject').value;
+    const name = document.getElementById('name').value;
+    const subject = document.getElementById('message').value;
+    window.open('mailto:okoasuquo@gmail.com?subject=${subject}&body=${body}');
+}
