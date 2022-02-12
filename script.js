44c0e1c04a60fa46e9b248bc341092cd206cd6fd
Kjasuquo <joseph.asuquo@decagon.dev>
2022-02-13 00:40:36 +0100

    Made my form functional using only HTML

--- script.js
+++
@@ -1,6 +0,0 @@
-function sendMail(){
-    const subject = document.getElementById('subject'.value);
-    const name = document.getElementById('name').value;
-    const subject = document.getElementById('message').value;
-    window.open('mailto:okoasuquo@gmail.com?subject=${subject}&body=${body}');
-}
